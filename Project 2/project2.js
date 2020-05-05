1429ef5a47cf38535f8c6c0cae109aba2451b06f
Vianny Lugo Aracena <vclugoar@Viannys-MacBook-Air.local>
2020-05-05 16:34:09 -0400

    resizing

--- Project 2/project2.js
+++ Project 2/project2.js
@@ -95,6 +95,33 @@ d3.csv("../data/feature_comp.csv", d3.autoType)
   init();
 });
 
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width")),
+      height = parseInt(svg.style("height")),
+      aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg.attr("viewBox", "0 0 " + width + " " + height)
+      .attr("perserveAspectRatio", "xMinYMid")
+      .call(resize);
+
+  // to register multiple listeners for same event type, 
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+      var targetWidth = parseInt(container.style("width"));
+      svg.attr("width", targetWidth);
+      svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
+
 
 /* INITIALIZING FUNCTION */
 function init() {
@@ -220,11 +247,14 @@ function init() {
   // create svg element for heatmap
   svg = d3
     .select("#d3-container2")
-    .attr("align","center")
+    //.attr("align","center")
     .append("svg")
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "-190 -400 1300 1600")
-    .classed("svg-content", true);
+    .attr("width", 960)
+    .attr("height", 1400)
+    .call(responsivefy);
+    // .attr("preserveAspectRatio", "xMinYMin meet")
+    // .attr("viewBox", "-190 -400 1300 1600")
+    // .classed("svg-content", true);
     // .append("svg")
     // .attr("width", width + margin.left + margin.right)
     // .attr("height", height + margin.top + margin.bottom)
@@ -272,7 +302,8 @@ function init() {
    .attr("height", height2 + margin1.top + margin1.bottom)
   //.attr("height", 200)
    .append("g")
-   .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")");
+   .attr("transform", "translate(" + margin1.left + "," + margin1.top + ")")
+   .call(responsivefy);
  
     // create svg element for energy
     barsvg5 = d3
